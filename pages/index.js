bec68b3fe0846f289f191751a8b71c9e316c86a8
Willamy Ungary <willamy.ungary@gmail.com>
2026-09-09 12:09:06 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     return (
         <>
-            <h1>Lana Meu Amor ❤️</h1>
+            <h1>Lana Amor da minha vida ❤️</h1>
             <p>Cada dia ao seu lado é um presente que transforma a minha vida. Seu sorriso ilumina meus dias e seu carinho é meu porto seguro. Te amo hoje, amanhã e para todo o sempre! ✨</p>
         </>
     )
