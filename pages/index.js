9469fe92c0381d6df40d99a864071a3658ab07bc
Willamy Ungary <willamy.ungary@gmail.com>
2026-09-11 22:38:51 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return (
         <>
             <h1>Lana, amor da minha vida❤️</h1>
-            <p>Hoje, 11/09/2026, nós completamos 26 anos de casamento. Quando olho para trás e vejo tudo o que já enfrentamos, tudo o que passamos juntos, momentos bons, momentos difíceis, momentos felizes e outros tristes, mas em tudo o Senhor nos sustentou e nos guiou! Amo ovcê de todo o meu amor e coração! ✨</p>
+            <p>Hoje, 11/09/2026, nós completamos 26 anos de casamento. Quando olho para trás e vejo tudo o que já enfrentamos, tudo o que passamos juntos, momentos bons, momentos difíceis, momentos felizes e outros tristes, mas em tudo o Senhor nos sustentou e nos guiou! Amo você de todo o meu amor e coração! ✨</p>
         </>
     )
 }
